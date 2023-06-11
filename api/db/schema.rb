a897d2fa5f25e0b01372ff47849faa2c6d3ef6a4
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2023-06-11 23:19:33 +0900

    postsテーブルにcontentカラムを追加

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2023_06_11_073354) do
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "title"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,19 +1,24 @@
 Post.create!(
   [
     {
-      title: 'Next.js + Ruby on Rails + Docker の環境構築'
+      title: 'Next.js + Ruby on Rails + Docker の環境構築',
+      content: "Next.js + Ruby on Rails + Docker の環境構築を行います。また、Next.js と Ruby on Rails を連携させる方法についても解説します。"
     },
     {
-      title: 'React Hooks でカスタムフックを作る'
+      title: 'React Hooks でカスタムフックを作る',
+      content: "React Hooks でカスタムフックを作る方法を紹介します。また、カスタムフックを作るメリットについても解説します。"
     },
     {
-      title: 'GraphQL と Apollo Client 入門'
+      title: 'GraphQL と Apollo Client 入門',
+      content: "GraphQL と Apollo Client の入門記事です。GraphQL と Apollo Client の基本的な使い方を紹介します。"
     },
     {
-      title: '【TypeScript4.3】Template Literal Types'
+      title: '【TypeScript4.3】Template Literal Types',
+      content: "TypeScript4.3 で追加された Template Literal Types について解説します。"
     },
     {
-      title: 'Tailwind CSS でダークモード実装'
+      title: 'Tailwind CSS でダークモード実装',
+      content: "Tailwind CSS でダークモードを実装する方法を紹介します。また、ダークモードの切り替え方法についても解説します。"
     },
   ]
 )
